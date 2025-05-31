df5da93024e5aabaea5e25f4f0367b4ad6b14588
Alberto García <malgardel@gmail.com>
2025-05-31 19:09:35 +0200

    Update builder

--- electron-builder.config.js
+++ electron-builder.config.js
@@ -37,42 +37,14 @@ module.exports = {
 		target: ['AppImage', 'deb'],
 		category: 'Audio',
 		icon: 'build/icons',
-		desktop: {
-			Name: 'Tuku',
-			Comment: 'Tuku Music Player',
-			Type: 'Application',
-			Exec: 'tuku',
-			Icon: 'tuku',
-			Categories: 'Audio;Player;',
-			StartupNotify: true,
-			StartupWMClass: 'Tuku',
-			Terminal: false,
-			Actions: [
-				{
-					Name: 'Play/Pause',
-					Exec: 'tuku --play-pause',
-					OnlyShowIn: 'Unity;GNOME;',
-				},
-				{
-					Name: 'Next',
-					Exec: 'tuku --next',
-					OnlyShowIn: 'Unity;GNOME;',
-				},
-				{
-					Name: 'Previous',
-					Exec: 'tuku --previous',
-					OnlyShowIn: 'Unity;GNOME;',
-				},
-			],
-		},
-		win: {
-			target: [
-				{
-					target: 'nsis',
-					arch: ['x64'],
-				},
-			],
-			icon: 'build/icons/icon.ico',
-		},
+	},
+	win: {
+		target: [
+			{
+				target: 'nsis',
+				arch: ['x64'],
+			},
+		],
+		icon: 'build/icons/icon.ico',
 	},
 }
