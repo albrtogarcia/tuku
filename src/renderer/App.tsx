9808b79626895cf275708289bc38ca5905529423
Alberto García <malgardel@gmail.com>
2025-12-10 11:18:13 +0100

    Grid layout

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -128,13 +128,13 @@ function App() {
 				<Queue audio={audio} />
 			</div>
 			<div className="container__library">
-				<div className="library-header">
+				<div className="library__header">
 					{/* TABS */}
-					<div className="library-tabs">
-						<button className={`library-tabs__button ${activeTab === 'albums' ? 'active' : ''}`} onClick={() => setActiveTab('albums')}>
+					<div className="tabs">
+						<button className={`tabs__button ${activeTab === 'albums' ? 'active' : ''}`} onClick={() => setActiveTab('albums')}>
 							Albums <small>({albums.length})</small>
 						</button>
-						<button className={`library-tabs__button ${activeTab === 'songs' ? 'active' : ''}`} onClick={() => setActiveTab('songs')}>
+						<button className={`tabs__button ${activeTab === 'songs' ? 'active' : ''}`} onClick={() => setActiveTab('songs')}>
 							Songs <small>({songs.length})</small>
 						</button>
 					</div>
@@ -144,7 +144,7 @@ function App() {
 				</div>
 
 				{/* TAB CONTENT */}
-				<div className="library-content">
+				<div className="library__body">
 					{activeTab === 'albums' && <AlbumsGrid albums={albums} setQueue={handleSetQueue} audio={audio} />}
 					{activeTab === 'songs' && <SongsList songs={filteredSongs} audio={audio} addToQueue={addToQueue} folderPath={folderPath} />}
 				</div>
